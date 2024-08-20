0929f51b82afef5815f97751bd838716dd498561
fatih bayram <fatihbayramybs@gmail.com>
2024-08-20 13:56:30 +0300

    layouts

--- twitter-clone/src/layouts/main/index.jsx
+++ twitter-clone/src/layouts/main/index.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Sidebar from "./sidebar";
+import RightBar from "./rightbar";
 
 function MainLayout() {
   return (
-    <div>
-      main layout! <Outlet />
+    <div className="w-[1265px] mx-auto flex">
+      <Sidebar />
+      <main className="flex-1 flex gap-[30px]">
+        <main className="flex-1 max-w-[600px] border-x border-[#2f336]">
+          <Outlet />
+        </main>
+        <RightBar />
+      </main>
     </div>
   );
 }
--- twitter-clone/src/pages/home/index.jsx
+++ twitter-clone/src/pages/home/index.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import MainLayout from "../../layouts/main";
 
 export default function Home() {
-  return <MainLayout>home component</MainLayout>;
+  return <div>home component</div>;
 }
