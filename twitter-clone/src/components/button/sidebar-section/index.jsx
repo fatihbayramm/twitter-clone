00f94e5aeb319a4a0d5f04d9ce16bf42fa73c75f
fatih bayram <fatihbayramybs@gmail.com>
2024-09-04 12:18:28 +0300

    color settings

--- twitter-clone/src/components/button/sidebar-section/index.jsx
+++ twitter-clone/src/components/button/sidebar-section/index.jsx
@@ -11,7 +11,7 @@ export default function SidebarSection({ title, children, more = "false" }) {
       {more && (
         <Link
           to={more}
-          className="h-[52px] flex rounded-b-2xl items-center px-4 text-[#1d9bf0] text-[15px] hover:bg-white/[0.03] transition-colors"
+          className="h-[52px] flex rounded-b-2xl items-center px-4 text-[color:var(--color-primary)] text-[15px] hover:bg-white/[0.03] transition-colors"
         >
           Daha fazla göster
         </Link>
